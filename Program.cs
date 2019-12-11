dd7f7e2d0dfd61fcbd52773aba11e2b80fbc59f2
olliik <54954916+olliik@users.noreply.github.com>
2019-12-12 01:05:11 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,35 +1,3 @@
-﻿/* LAHDE: http://web.fi.uba.ar/~jvillca/hd/public/books/Data_Structures_and_Algorithms_in_Java_2nd_Edition.pdf s.419
-
-    1. Make a Node object (as seen in tree.java) for each character used in the
-    message. For our Susie example that would be nine nodes. Each node has two
-    data items: the character and that character’s frequency in the message. Table
-    8.4 provides this information for the Susie message.
-
-    2. Make a tree object for each of these nodes. The node becomes the root of the
-    tree.
-
-    3. Insert these trees in a priority queue (as described in Chapter 4). They are
-    ordered by frequency, with the smallest frequency having the highest priority.
-    That is, when you remove a tree, it’s always the one with the least-used
-    character.
-
-    Now do the following:
-    1. Remove two trees from the priority queue, and make them into children of a
-    new node. The new node has a frequency that is the sum of the children’s
-    frequencies; its character field can be left blank.
-
-    2. Insert this new three-node tree back into the priority queue.
-
-    3. Keep repeating steps 1 and 2. The trees will get larger and larger, and there will
-    be fewer and fewer of them. When there is only one tree left in the queue, it is
-    the Huffman tree and you’re done
-
-
-    The characters with the highest counts should be coded with a small number of bits.
-
-    */
-
-
 using System;
 using System.Collections.Generic;
 
